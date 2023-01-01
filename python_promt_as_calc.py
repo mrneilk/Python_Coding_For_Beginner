4481281f2b40d7cf86afcc87d4fba2d93e4f9ef3
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-01 22:59:58 +0530

    Create python_promt_as_calc.py
    This is the first exercise for complete beginners in python.

---
+++ python_promt_as_calc.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+
+In the console of spyder environment of anaconda perform the following operations and see what are the outputs and note down errors in the statements if any are found..
+or you can also use jupyter nootbook or any other python interface you like..
+1) In [1]: 6 * 3
+   Out[1]: 18
+
+2) In [2]: 3 ** 3
+   Out[2]: 27
+
+3) In [3]: 6 + 2 * 4
+   Out[3]: 14
+
+4) In [4]: (6 + 2) * 4
+   Out[4]: 32
+
+5) In [5]: 5 - 3 - 3
+   Out[5]: -1
+
+6) In [6]: k = 5 - (3 - 3)
+
+7) In [7]: k
+   Out[7]: 5
+
+8) In [8]: 8 % 3
+   Out[8]: 2
+
+9) In [9]: 3.0 * 0.5
+   Out[9]: 1.5
+
+10) In [10]: 2 < 5
+    Out[10]: True
+
+11) In [11]: 4 < 3 or 6 > 7
+    Out[11]: False
+
+12) In [12]: 3 <= 3
+    Out[12]: True
+
+13) In [13]: 4 != 5
+    Out[13]: True
+
+"""
