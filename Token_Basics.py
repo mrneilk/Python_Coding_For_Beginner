b4ab20b32530db66f12d01a2d8331243b8618148
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-02 21:09:00 +0530

    Create Token_Basics.py
    Perform the operations on the Spyder console and check the results.
    This is related to usage of tokens in Python programming language.

---
+++ Token_Basics.py
@@ -0,0 +1,98 @@
+Python 3.9.13 (main, Aug 25 2022, 23:51:50) [MSC v.1916 64 bit (AMD64)]
+Type "copyright", "credits" or "license" for more information.
+
+IPython 7.31.1 -- An enhanced Interactive Python.
+
+In [1]: abc123 = 25
+
+In [2]: abc123
+Out[2]: 25
+
+In [3]: abc123*2
+Out[3]: 50
+
+In [4]: print(abc123)
+25
+
+In [5]: break = 10
+  File "C:\Users\mrnei\AppData\Local\Temp\ipykernel_16076\3274487030.py", line 1
+    break = 10
+          ^
+SyntaxError: invalid syntax
+
+
+In [6]: Break = 10
+
+In [7]: break
+  File "C:\Users\mrnei\AppData\Local\Temp\ipykernel_16076\668683560.py", line 1
+    break
+    ^
+SyntaxError: 'break' outside loop
+
+
+In [8]: print(break)
+  File "C:\Users\mrnei\AppData\Local\Temp\ipykernel_16076\1126903501.py", line 1
+    print(break)
+          ^
+SyntaxError: invalid syntax
+
+
+In [9]: print(abc123 * 2)
+50
+
+In [10]: 123abc =20
+  File "C:\Users\mrnei\AppData\Local\Temp\ipykernel_16076\345807851.py", line 1
+    123abc =20
+       ^
+SyntaxError: invalid syntax
+
+
+In [11]: val1=12.25
+
+In [12]: val1
+Out[12]: 12.25
+
+In [13]: print(val1)
+12.25
+
+In [14]: elif = 'abc'
+  File "C:\Users\mrnei\AppData\Local\Temp\ipykernel_16076\2840396878.py", line 1
+    elif = 'abc'
+    ^
+SyntaxError: invalid syntax
+
+
+In [15]: Name = "Navraj"
+
+In [16]: Name
+Out[16]: 'Navraj'
+
+In [17]: print(Name)
+Navraj
+
+In [18]: Name*2
+Out[18]: 'NavrajNavraj'
+
+In [19]: Name *2
+Out[19]: 'NavrajNavraj'
+
+In [20]: String1 = "text"
+
+In [21]: len(String1)
+Out[21]: 4
+
+In [22]: A = None
+
+In [23]: A
+
+In [24]: print(A)
+None
+
+In [25]: Chk = True
+
+In [26]: Chk
+Out[26]: True
+
+In [27]: print(Chk)
+True
+
