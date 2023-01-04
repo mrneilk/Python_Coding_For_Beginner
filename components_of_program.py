626ead6e3e8fea30e5c4f0399ab19ea3632d16ae
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-04 21:38:39 +0530

    Create components_of_program.py
    Simple python program for understanding components of a python program

---
+++ components_of_program.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 21:24:56 2023
+
+@author: mrneilk
+"""
+
+#Components of a program
+
+# Fuction 1
+def MultiplesofSeven():
+    print("7*1=",(7*1))
+    print("7*2=",(7*2))
+    print("7*3=",(7*3))
+    print("7*4=",(7*4))
+    print("7*5=",(7*5))
+
+# Main Function
+print("Hello")
+print("MultipleofSeven() function will print 5 multiples of seven")
+MultiplesofSeven()  # calling MultipleofSeven() function
+
+# Analyze the above program and find out the different componts used in this.
+# Character set
+# Tokens
+# Expressions
+# Statements
+# Simple Input and Output
