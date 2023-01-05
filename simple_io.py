2fe2369aba6d56f63ccf0e9e019933de5f33ba5a
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-05 22:16:02 +0530

    Create simple_io.py
    Program that has simple input and output.

---
+++ simple_io.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 22:11:47 2023
+
+@author: mrneilk
+"""
+
+x = int(input('Enter a number'))      #input an integer in variable x
+print('x =', x)                       #print the variable x
+print(x,x*2,x*3,x*4,x*5)              #print 5 multiples of x
