7bdb07adecd83599d79b284fa3e0dd5c87570a17
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-06 22:18:32 +0530

    Create data_types.py
    Resources for data type in python programming language

---
+++ data_types.py
@@ -0,0 +1,3 @@
+# i am attaching a list of resources for this work where we can find information about data types in python
+# 1)https://docs.python.org/3/library/datatypes.html
+# 2)https://www.programiz.com/python-programming/variables-datatypes
