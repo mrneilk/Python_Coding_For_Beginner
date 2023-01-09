75ce52daf0a8d056d82ff4435701752b916f2b9f
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-09 21:40:25 +0530

    Create sphere.py

---
+++ sphere.py
@@ -0,0 +1,5 @@
+# Program to find surface area, S = 4 * pi * r^2
+# And Volume, V = 4/3 * pi * r^3
+r = float(input('Enter the radius:'))
+print('S=', 4 * 3.142 * r**2)
+print('V=', (4/3) * 3.142 * r**3)
