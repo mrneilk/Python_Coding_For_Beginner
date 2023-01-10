1f23ad2251ed335e19b9f8204668db21e2cd84ff
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-10 20:50:14 +0530

    Update EXPRESSION_PRO_1.py

--- EXPRESSION_PRO_1.py
+++ EXPRESSION_PRO_1.py
@@ -1,1 +1,5 @@
 # Program to convert height form centimetres to feet and inches
+h1 = float(input('Enter height in cm:'))
+feet = int(h1/30.48)
+inches = int((h1/2.54)%12)
+print('Your Height is ',feet,' feet ', inches, ' inches')
