6b34d8837ead04f96e4dd9a57f27cde2a1517b9d
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-15 18:27:26 +0530

    Create flow_of_control_1.py

---
+++ flow_of_control_1.py
@@ -0,0 +1,22 @@
+#program to input a digit and print it in words.
+n=int(input('enter a digit:'))
+if(n==1):
+  print('ONE')
+if(n==2):
+  print('TWO')
+if(n==3):
+  print('THREE')
+if(n==4):
+  print('FOUR')
+if(n==5):
+  print('FIVE')
+if(n==6):
+  print('SIX')
+if(n==7):
+  print('SEVEN')
+if(n==8):
+  print('EIGHT')
+if(n==9):
+  print('NINE')
+if(n==0):
+  print('ZERO')
