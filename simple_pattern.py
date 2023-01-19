4458301af9594d2622f6232dfec3588a7d420b7a
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-19 22:18:37 +0530

    Update simple_pattern.py

--- simple_pattern.py
+++ simple_pattern.py
@@ -1,3 +1,9 @@
 # print AAAAAAAAAABBBBBBBCCCCCCDDDDD using 4 for loops
 for i in range(10):
-  print('A')
+  print('A',end="")
+for i in range(7):
+  print('B',end="")
+for i in range(6):
+  print('C',end="")
+for i in range(6):
+  print('D',end="")
