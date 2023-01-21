a2c4c67ca4b4e6e5469a605a1a62dec4016335db
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-21 21:11:16 +0530

    Update Fizz_Buzz.py

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,5 +1,6 @@
-# Simple FizzBuzz Program , if number is divisible by 3 and 5 print FizzBuzz if number divisible by only 3 print Buzz and 
+# Simple FizzBuzz Program , if number is divisible by 3 and 5 print FizzBuzz if number divisible by only 3 print Fizz and 
 # if number only divisible by 5 print Buzz if not any one of them print the number
+# range of number is from 1 to 100
 for i in range(1,100):
   if (i%3==0 and i%5==0):
     print("FizzBuzz")
