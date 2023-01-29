d560c5c08e20cbe476fc79300cc91dbdef170c15
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-29 20:47:29 +0530

    Update Z_Pattern_1.py

--- Z_Pattern_1.py
+++ Z_Pattern_1.py
@@ -6,10 +6,10 @@
 # #
 # ######
 # Print The following Pattern
-n=int(input('Enter N:'))
-for i in range (n):    
+n=int(input('Enter N:'))    # input the size
+for i in range (n):     
     for j in range (n):
-        if(j==n-i or i==0 or i==n-1):
+        if(j==n-i or i==0 or i==n-1):       # Print in first and last rows and Print in each row with col=size-row number
             print('#',end='')
         else:
             print(' ',end='')
