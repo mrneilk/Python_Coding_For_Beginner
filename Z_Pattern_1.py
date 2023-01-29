591d8281140f4d0db5d4bc2687340a1dd0d9ccb5
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-29 20:46:13 +0530

    Update Z_Pattern_1.py

--- Z_Pattern_1.py
+++ Z_Pattern_1.py
@@ -5,3 +5,14 @@
 #  #
 # #
 # ######
+# Print The following Pattern
+n=int(input('Enter N:'))
+for i in range (n):    
+    for j in range (n):
+        if(j==n-i or i==0 or i==n-1):
+            print('#',end='')
+        else:
+            print(' ',end='')
+    print('\n')
+    
+    
