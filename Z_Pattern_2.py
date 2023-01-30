0293925318196e20e37c2813f99586f3d3d1f74e
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-01-30 19:00:21 +0530

    Update Z_Pattern_2.py

--- Z_Pattern_2.py
+++ Z_Pattern_2.py
@@ -9,7 +9,7 @@
 n=int(input('Enter N:'))    # input the size
 for i in range (n):     
     for j in range (n):
-        if(j==i or i==0 or i==n-1):       # Print in first and last rows and Print in each row with col=size-row number
+        if(j==i or i==0 or i==n-1):       # Print in first and last rows and Print in each row with col number = row number
             print('#',end='')
         else:
             print(' ',end='')
