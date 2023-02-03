c9932bf8e8f4d4b33e81026052cf4536a138a4a4
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-02-03 21:01:19 +0530

    Update first_10_prime.py

--- first_10_prime.py
+++ first_10_prime.py
@@ -1,12 +1,12 @@
 # Program to First ten prime Numbers using loops
 # Reference: https://www.geeksforgeeks.org/generate-and-print-first-n-prime-numbers/
-import math
+import math                                                 # Import Math Library for square root, and Floor function 
 X = 0
 i = 2
-flag = False
+flag = False                                                # Declare a Boolean variable flag False
 while(X < 10):
   flag = True
-  for j in range(2, math.floor(math.sqrt(i)) + 1):
+  for j in range(2, math.floor(math.sqrt(i)) + 1):          # Main Conditional loop, checks for prime number
     if (i%j == 0):
       flag = False
       break
