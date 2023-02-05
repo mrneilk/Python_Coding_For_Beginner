b266542a3eba5e507b79a96a082ab302b56eb8b3
mrneilk <navrajkhanal2001@gmail.com>
2023-02-05 21:11:56 +0530

    File Update

--- Series_And_Sequence_Problems/series_1.py
+++ Series_And_Sequence_Problems/series_1.py
@@ -1,1 +1,6 @@
 # Series and Sequence 
+# Print Series Starting from 1, 1, 2
+# Every Next Number is defined as (prev1*prev2 + prev3)
+# ....prev3, prev2, prev1, Nth Term
+# Basically
+# 1, 2, 2, 3, 7, 23, 164 ...
