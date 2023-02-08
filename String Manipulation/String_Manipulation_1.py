39af594740b84178bff27c22cbdf2eaba98be484
mrneilk <navrajkhanal2001@gmail.com>
2023-02-08 19:02:42 +0530

    update file 1 for strings

--- String Manipulation/String_Manipulation_1.py
+++ String Manipulation/String_Manipulation_1.py
@@ -1,1 +1,8 @@
 # Write a loop that print out index of every 'i' in 'Mississippi'
+word = "Mississippi"
+ind = 0
+for char in word:
+    if char == 'i':
+        ind = word.index(char,ind+1)
+        print(ind)
+        
