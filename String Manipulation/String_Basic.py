835eea36e3177e50ede57dfdac9dbe1c1c94d7f9
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-02-09 22:28:30 +0530

    Create String_Basic.py

---
+++ String Manipulation/String_Basic.py
@@ -0,0 +1,16 @@
+# References Progamiz.com
+# create string type variables
+
+name = "Python"
+print(name)
+
+message = "I love Python."
+print(message)
+
+# Accessing String Index
+greet = 'hello'
+
+# access 1st index element
+print(greet[1]) # "e"
+
+# In Python, strings are immutable. That means the characters of a string cannot be changed.
