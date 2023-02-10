a678c643be8416d7e02eda5faa5ecc46100ef1ca
mrneilk <navrajkhanal2001@gmail.com>
2023-02-10 21:04:59 +0530

    update new file

--- String Manipulation/String_Manipulation_2.py
+++ String Manipulation/String_Manipulation_2.py
@@ -1,1 +1,8 @@
 # input a word and print "yes it starts!!" if the word starts with '0' '1' '2' ... '9'.
+word = input('Enter the word')
+try:
+    num = int(word[0])
+    if num >= 0 and num <= 9:
+        print("Yes It Starts")
+except:
+    print("No")
