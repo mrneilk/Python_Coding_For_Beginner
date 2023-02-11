a9bb5dc301ede00a6ed0d88d049c5e1550022309
mrneilk <navrajkhanal2001@gmail.com>
2023-02-11 23:08:30 +0530

    update password program file

--- String Manipulation/Password_Checker.py
+++ String Manipulation/Password_Checker.py
@@ -1,2 +1,8 @@
 # Input a password
 # if its length is more than 6 and starts with a digit it is valid else it is invalid
+password = input('Enter Password')
+if len(password) > 6:
+    if password[0].isdigit():
+        print('Password accepted')
+else:
+    print('Invalid Password')
