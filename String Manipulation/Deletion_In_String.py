d9e1ca4e9fe333982bf1a6d5c705ea6e1fea9f28
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2023-02-12 22:11:32 +0530

    Update Deletion_In_String.py

--- String Manipulation/Deletion_In_String.py
+++ String Manipulation/Deletion_In_String.py
@@ -1,1 +1,19 @@
 # Reference GeeksForGeeks
+# Python Program to Delete
+# characters from a String
+ 
+String1 = "Hello, I'm a Geek"
+print("Initial String: ")
+print(String1)
+ 
+# Deleting a character
+# of the String
+String2 = String1[0:2] + String1[3:]
+print("\nDeleting character at 2nd Index: ")
+print(String2)
+
+# Initial String: 
+# Hello, I’m a Geek
+
+# Deleting character at 2nd Index: 
+# Helo, I’m a Geek
